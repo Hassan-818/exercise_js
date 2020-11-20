15535c48de9cd57c82254dbca5db7c88ce38a730
Hassan-818 <almhassan818@gmail.com>
2020-11-20 20:11:07 +0530

    array

---
+++ intex.js
@@ -0,0 +1,49 @@
+// const, let, var, globel
+
+function getName(){
+     var name="hassan";
+     console.log("how are you?:"+ name);
+
+}
+
+getName()
+
+console.log('Hi!'+name)
+
+function myName(){
+    x="mohamed"
+    console.log(x);
+}
+
+myName()
+ 
+console.log(x);
+
+function name(){
+    if(true){
+        let grade = 'A';
+        console.log(grade);
+    }
+}
+
+name()
+
+function fName(){
+    if(true){
+        const grade = 'A';
+        
+        console.log(grade);
+    }
+}
+
+fName()
+
+
+// arithmatic operators
+  //+, -, *, /, %, ++, --,**
+  // Assignment operators
+  
+  //comparison operators
+
+  
+ 
